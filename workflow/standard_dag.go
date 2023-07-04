676792633ff109da03f9274d2d64b94eae830d48
Zhao vistart <i@vistart.me>
2023-07-04 15:57:02 +0800

    start StandardDAG

--- workflow/standard_dag.go
+++ workflow/standard_dag.go
@@ -1,6 +1,10 @@
 package workflow
 
-import "context"
+import (
+	"context"
+	"log"
+	"sync"
+)
 
 type StandardDAGInterface[TInput, TOutput any] interface {
 	SimpleDAGInitInterface
@@ -10,4 +14,91 @@ type StandardDAGInterface[TInput, TOutput any] interface {
 	CloseWorkflow()
 	Execute(ctx context.Context, input *TInput) *TOutput
 	RunOnce(ctx context.Context, input *TInput) *TOutput
+}
+
+type StandardDAG[TInput, TOutput any] struct {
+	// channels stores all channels of this directed acyclic graph. The key of the map is the channel name.
+	channels         map[string]chan any
+	channelInput     string
+	channelOutput    string
+	workflowTransits []*SimpleDAGWorkflowTransit
+	logger           *log.Logger
+	StandardDAGInterface[TInput, TOutput]
+}
+
+func NewStandardDAG[TInput, TOutput any]() *StandardDAG[TInput, TOutput] {
+	return &StandardDAG[TInput, TOutput]{
+		logger: log.Default(),
+	}
+}
+
+func (d *StandardDAG[TInput, TOutput]) BuildWorkflow(ctx context.Context) error {
+	// Build Input
+	if len(d.channelInput) == 0 {
+		return ErrChannelInputEmpty
+	}
+
+	// Build Output
+	if len(d.channelOutput) == 0 {
+		return ErrChannelOutputEmpty
+	}
+
+	// Build Transits
+	if len(d.workflowTransits) == 0 {
+		return nil
+	}
+	for _, t := range d.workflowTransits {
+		for _, name := range t.channelInputs {
+			if _, existed := d.channels[name]; !existed {
+				return ErrChannelNotExist
+			}
+		}
+		for _, name := range t.channelOutputs {
+			if _, existed := d.channels[name]; !existed {
+				return ErrChannelNotExist
+			}
+		}
+	}
+	for _, t := range d.workflowTransits {
+		go func(t *SimpleDAGWorkflowTransit) {
+			results := d.BuildWorkflowOutput(ctx, t.channelInputs...)
+			var result, err = t.worker(*results...)
+			if err != nil {
+				d.logger.Printf("worker[%s] error(s) occurred: %s\n", t.name, err.Error())
+			}
+			d.BuildWorkflowInput(result, t.channelOutputs...)
+		}(t)
+	}
+	return nil
+}
+
+func (d *StandardDAG[TInput, TOutput]) Execute(ctx context.Context, input *TInput) *TOutput {
+	err := d.BuildWorkflow(ctx)
+	if err != nil {
+		return nil
+	}
+	var results *TOutput
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		r := d.BuildWorkflowOutput(ctx, d.channelOutput)
+		if r, ok := (*r)[0].(TOutput); !ok {
+			t := new(TOutput)
+			var e = SimpleDAGValueTypeError{actual: r, expect: t}
+			d.logger.Println(e)
+			results = nil
+			return
+		} else {
+			results = &r
+		}
+	}()
+	d.BuildWorkflowInput(*input, d.channelInput)
+	wg.Wait()
+	return results
+}
+
+func (d *StandardDAG[TInput, TOutput]) RunOnce(ctx context.Context, input *TInput) *TOutput {
+	defer d.CloseWorkflow()
+	return d.Execute(ctx, input)
 }
--- workflow/standard_dag_test.go
+++ workflow/standard_dag_test.go
@@ -1,1 +1,8 @@
 package workflow
+
+import "testing"
+
+func TestNewStandardDAG(t *testing.T) {
+	dag := NewStandardDAG[int, int]()
+	t.Log(dag)
+}
